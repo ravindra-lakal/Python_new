8a7cadac4e56d8f74bf0396484a0feab5c894d02
sagar30051991 <sagarshiragawakar@gmail.com>
2016-03-16 13:13:34 +0530

    Added mak.py

---
+++ mak.py
@@ -0,0 +1,59 @@
+
+#1 Find the last element of a list.
+
+list = ['a','b','c','d']
+#validation list =[]
+print list[-1:]
+
+#print "list[4]",list[4]
+
+
+
+#2 Find the last but one element of a list.
+
+list =['z','w','e','i','t','l','e','t','z','t','e','s']
+#validation list=[1]
+print list[-2:-1]
+
+
+ 
+#3 1.03 (*) Find the K'th element of a list.
+
+list = ['a','b','c','d','e','f','g']
+print list[3]
+
+
+
+
+#4
+somelist = ["a", "b", "c", "d"];
+print len(somelist)
+
+
+
+#5 reverse the list
+
+list = ['a','b','c','d']
+
+#print list[::-1]
+
+list.reverse()
+print list
+
+
+#5 Addition of 2 numbers
+
+a= input("enter no")
+
+# b=raw_input("enter no")
+
+print a
+
+
+#6 check string Find out whether a list is a palindrome
+# name = raw_input("Enter a string: ")
+# rev_str = reversed(name)
+# if list(name) == list(rev_str):
+#    print("It is palindrome")
+# else:
+#    print("It is not palindrome")
